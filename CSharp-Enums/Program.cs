a6f9a736a6356b043395cecf77dc64eaef5c4fb5
Jake <jakenguyen0118@gmail.com>
2021-04-13 12:20:17 -0400

    Add project files.

---
+++ CSharp-Enums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharp_Enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+        // if values are not set, the first value will be 0 and incremented by 1
+        // best practice to always set values for enums
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
